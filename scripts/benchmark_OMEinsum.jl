1c3f02b33a46ba92a809f675d0ec1cfae4f44a78
GiggleLiu <cacate0129@gmail.com>
2022-01-02 04:56:30 +0000

    benchmark omeinsum update

--- scripts/benchmark_OMEinsum.jl
+++ scripts/benchmark_OMEinsum.jl
@@ -1,5 +1,5 @@
 using OMEinsum, OMEinsumContractionOrders
 using CUDA, BenchmarkTools
 
-optcode_loaded = readjson("test.json")
-display(@benchmark CUDA.@sync optcode_loaded([CUDA.rand(fill(2, length(ix))...) for ix in getixsv(optcode_loaded)]...))
+optcode_loaded = readjson("tensornetwork.json")
+display(@benchmark (CUDA.@sync optcode_loaded([CUDA.rand(fill(2, length(ix))...) for ix in getixsv(optcode_loaded)]...)) samples=3)
