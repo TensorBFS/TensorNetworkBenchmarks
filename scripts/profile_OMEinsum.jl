aac87670b0287db44dc05e9b54f9809bf637f51b
GiggleLiu <cacate0129@gmail.com>
2022-01-05 02:32:25 +0000

    update benchmarks

---
+++ scripts/profile_OMEinsum.jl
@@ -0,0 +1,13 @@
+using OMEinsum, OMEinsumContractionOrders
+using CUDA, Profile
+
+optcode_loaded = readjson("tensornetwork.json")
+@show timespacereadwrite_complexity(optcode_loaded.eins, uniformsize(optcode_loaded, 2))
+GC.gc()
+#Profile.@profile Array(optcode_loaded([CUDA.fill(0.5^(0.4), (fill(2, length(ix))...,)) for ix in getixsv(optcode_loaded)]...))
+@show Array(OMEinsum.fast_contract(optcode_loaded.eins,([CUDA.fill(0.5^(0.4), (fill(2, length(ix))...,)) for ix in getixsv(optcode_loaded)]...,)))
+Profile.@profile Array(OMEinsum.fast_contract(optcode_loaded.eins,([CUDA.fill(0.5^(0.4), (fill(2, length(ix))...,)) for ix in getixsv(optcode_loaded)]...,)))
+Profile.clear()
+GC.gc()
+Profile.@profile Array(OMEinsum.fast_contract(optcode_loaded.eins,([CUDA.fill(0.5^(0.4), (fill(2, length(ix))...,)) for ix in getixsv(optcode_loaded)]...,)))
+Profile.print(mincount=10, recur=:flat)
