916a6782bee63bfcf3ec94b610199fe0cf1063a6
GiggleLiu <cacate0129@gmail.com>
2022-01-12 21:29:31 +0000

    update benchmarksg

--- profile/nsys_OMEinsum.jl
+++ profile/nsys_OMEinsum.jl
@@ -2,13 +2,8 @@ using OMEinsum, OMEinsumContractionOrders
 using CUDA, BenchmarkTools
 
 optcode_loaded = readjson(joinpath(dirname(@__DIR__), "scripts", "tensornetwork_permutation_optimized.json"))
-@show timespacereadwrite_complexity(optcode_loaded.eins, uniformsize(optcode_loaded, 2))
+@show timespacereadwrite_complexity(optcode_loaded, uniformsize(optcode_loaded, 2))
 xs = [CUDA.fill(Float32(0.5^(0.4)), (fill(2, length(ix))...,)) for ix in getixsv(optcode_loaded)]
-GC.gc()
 @show Array(optcode_loaded(xs...; active_free=true))
-GC.gc()
-CUDA.synchronize()
 CUDA.@profile Array(optcode_loaded(xs...; active_free=true))
-GC.gc()
-CUDA.synchronize()
 CUDA.@profile Array(optcode_loaded(xs...; active_free=true))
--- profile/profile_OMEinsum.jl
+++ profile/profile_OMEinsum.jl
@@ -2,7 +2,7 @@ using OMEinsum, OMEinsumContractionOrders
 using CUDA, Profile
 
 optcode_loaded = readjson(joinpath(dirname(@__DIR__), "scripts", "tensornetwork_permutation_optimized.json"))
-@show timespacereadwrite_complexity(optcode_loaded.eins, uniformsize(optcode_loaded, 2))
+@show timespacereadwrite_complexity(optcode_loaded, uniformsize(optcode_loaded, 2))
 GC.gc()
 @show Array(optcode_loaded([CUDA.fill(Float32(0.5^(0.4)), (fill(2, length(ix))...,)) for ix in getixsv(optcode_loaded)]...))
 Profile.@profile Array(optcode_loaded([CUDA.fill(Float32(0.5^(0.4)), (fill(2, length(ix))...,)) for ix in getixsv(optcode_loaded)]...))
